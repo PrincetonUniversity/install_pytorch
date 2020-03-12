4605646329c5f820288ba75d8227e1c24182a5cc
jdh4 <52128661+jdh4@users.noreply.github.com>
2020-03-11 22:34:24 -0400

    Delete mnist_download.py

--- mnist_download.py
+++
@@ -1,6 +0,0 @@
-import torchvision
-
-# Compute nodes do not have internet so download the data in advance
-
-_ = torchvision.datasets.MNIST('data', train=True, transform=None,
-                               target_transform=None, download=True)
